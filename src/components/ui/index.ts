a1aafc07d8f7635e746faf1343461d66121e721f
Dominika <dominika.sobiecka@gmail.com>
2023-07-12 00:16:44 +0200

    index.js

---
+++ src/components/ui/index.ts
@@ -0,0 +1,9 @@
+export { Button } from "./Button/Button";
+export { Dropdown } from "./Dropdown/Dropdown";
+export { Input } from "./Input/Input";
+export { ListElement } "./ListElement/ListElement";
+export { Navbar } from "";
+export { ProgressBar } from "";
+export { Slider } from "";
+export { Switch } from "";
+
