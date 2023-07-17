ed5e9a20c14c8334be7ebbd68b76f199631efd48
Dominika <dominika.sobiecka@gmail.com>
2023-07-17 23:55:42 +0200

    Breadcrumb component

---
+++ src/components/ui/Breadcrumb/Breadcrumb.tsx
@@ -0,0 +1,39 @@
+// Breadcrumb.tsx
+
+import React from "react";
+import { Link } from "react-router-dom";
+
+type BreadcrumbItem = {
+  text: string;
+  path: string;
+};
+
+type BreadcrumbProps = {
+  items: BreadcrumbItem[];
+};
+
+export const Breadcrumb = ({ items }: BreadcrumbProps) => {
+  return (
+    <nav aria-label="breadcrumb">
+      <ol className="breadcrumb">
+        {items.map((item, index) => {
+          const isLastItem = index === items.length - 1;
+
+          return isLastItem ? (
+            <li
+              key={item.path}
+              className="breadcrumb-item active"
+              aria-current="page"
+            >
+              {item.text}
+            </li>
+          ) : (
+            <li key={item.path} className="breadcrumb-item">
+              <Link to={item.path}>{item.text}</Link>
+            </li>
+          );
+        })}
+      </ol>
+    </nav>
+  );
+};
